bdcc36cbfda35530641249ed35d0ae743dcd6087
Holger Hans Peter Freyther <holger@freyther.de>
2012-05-23 20:54:28 +0200

    [INTERNAL] Introduce an ant file and a test suite

---
+++ JTourBus/test/src/de/fu_berlin/inf/jtourbus/TestSuite.java
@@ -0,0 +1,10 @@
+package de.fu_berlin.inf.jtourbus;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({_TestBusStopJavaDoc.class})
+public class TestSuite {
+
+}
